7bc709dc78bbd5dbf1810e3b57cf7cf94addd55c
Drandria <14randrianandrasana@gmail.com>
2024-06-10 22:43:48 +0300

    ajout page, ajout article

---
+++ products.js
@@ -0,0 +1,45 @@
+let productPageContent = `
+    <div id="path">
+    <ul id="location"></ul>
+    </div>
+
+
+    <main>
+    <div id="message">
+        <p>Votre boutique en ligne vous propose l'intégralité de ses Chaussures parmis Tous les produits. 
+        Commandez en ligne et faites vous livrer ou bien profitez de notre service Click and Collect afin 
+        de venir récuperer votre commande dans votre boutique vetement femme en ligne | Joya le store</p>
+    </div>
+
+    <div class="articles" id="vente"></div>
+
+    </main>
+`
+
+let pageDeConnection = `
+    <main>
+        <div id="connectContent">
+            <h2 id="identifiantH2">Connectez-vous à votre compte</h2>
+            <div id="formulaire">
+                <form action="" id="idForm">
+                    <div>
+                        <label for="email">E-mail</label>
+                        <input type="email" name="email" id="email">
+                    </div>
+                    <div>
+                        <label for="password">Mot de passe</label>
+                        <div id="emailInput">
+                            <input type="password" name="password" id="password">
+                            <button>Montrer</button>
+                        </div>
+                    </div>
+                    <p>Mot de passe oublié ?</p>
+                    <button type="submit">Se connecter</button>
+                </form>
+                <div>
+                    <p>Pas de compte? Créez un ici</p>
+                </div>
+            </div>
+        </div>    
+    </main>
+`
--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 fetch('data.json')
     .then(res => res.json())
     .then(data => {
-        let accueil = data.accueil
+        let accueil = data.accueil.produits
+
+        // données Panier
+        var panier = 0
+        var listAchat = []
 
         const container = document.querySelector('.articles')
 
+        // ajout dans les nouveaux produits
         let star = ""
 
         accueil.forEach(element => {
@@ -15,7 +20,7 @@ fetch('data.json')
             }
 
             container.innerHTML += `
-                <div class="scarte">
+                <div class="scarte" id="${element.id}">
                     <img src="${element.image}" alt="" class="scarteImg">
                     <a href="#">${element.nom}</a>
                     <p>${element.prix} €</p>
@@ -25,4 +30,217 @@ fetch('data.json')
                 </div>
             `
         });
+
+        const scarte = document.querySelectorAll('.scarte')
+
+        scarte.forEach(scarteElt => {
+            scarteElt.addEventListener('click', function () {
+                console.log(scarteElt.id)
+                accueil.forEach(element => {
+                        if (element.id === parseInt(scarteElt.id)){
+                        variable.innerHTML = `
+                            <div id="path">
+                            <ul id="location"></ul>
+                            </div>
+                        
+                        
+                            <main>
+                                <div id="infoElement">
+                                    <img src="${element.image}" alt="">
+                                    <div id="descri">
+                                        <div id="about">
+                                            <h1>${element.nom}</h1>
+                                            <h2>${element.prix}</h2>
+                                        </div>
+                                        <form action="" id="quantity">
+                                            <div>
+                                                <p>Taille</p>
+                                                <select name="" id="">
+                                                    <option value="">S</option>
+                                                    <option value="">L</option>
+                                                    <option value="">M</option>
+                                                </select>
+                                            </div>
+                                            <div>
+                                                <p>Quantité</p>
+                                                <input id="inputQuantity" type="number" name="" id="" min=0 value="0">
+                                            </div>
+                                            <p id="button">AJOUTER AU PANIER</p>
+                                        </form>
+                                    </div>
+                                </div>
+                            </main>
+                        `
+                        const button = document.querySelector('#button')
+
+                                button.addEventListener('click', function () {
+
+                                    let articleNbr = parseInt(document.querySelector('#inputQuantity').value)
+
+                                    for (let i = 0; i < articleNbr; i++){
+                                        listAchat.push({"nom":element.nom, "prix":element.prix})
+                                    }
+
+                                    const nbrPanier = document.querySelector('#panierFlottant').querySelector('p')
+                                    const nbrPanier2 = document.querySelector('#nbr')
+
+                                    nbrPanier.innerText = listAchat.length
+                                    nbrPanier2.innerText = listAchat.length
+                                })
+                        }
+                })
+            })
+        })
+
+        // changement de page
+
+        const page = document.querySelector('nav').querySelectorAll('.navSection')
+        const variable = document.querySelector('#variable')
+
+        let path = ""
+
+        page.forEach(content => {
+
+            content.addEventListener('click', function () {
+                variable.innerHTML = productPageContent
+                let article = content.attributes.value.nodeValue
+                const location = document.querySelector('#location')
+
+                //PATH
+                path = ""
+                data[article].path.forEach(loc => {
+                    location.innerHTML += `<li>/${loc}</li>`
+                })
+
+                //produits
+                data[article].produits.forEach(element => {
+                    star = ""
+    
+                    for(let i = 0; i < element.note; i++){
+                        star += `<img src="./image/étoile.png" alt="" class="star">`
+                    }
+    
+                    document.querySelector('#vente').innerHTML += `
+                        <div class="gcarte" id="${element.id}">
+                            <img src="${element.image}" alt="" class="scarteImg">
+                            <a href="#">${element.nom}</a>
+                            <p>${element.prix} €</p>
+                            <div class="note">
+                                ${star}
+                            </div>
+                        </div>
+                    `
+                })
+
+                //page Element selectioner
+
+                const gcarte = document.querySelectorAll('.gcarte')
+
+                gcarte.forEach(gcarteElt => {
+                    gcarteElt.addEventListener('click', function () {
+                        data[article].produits.forEach(element => {
+                             if (element.id === parseInt(gcarteElt.id)){
+                                variable.innerHTML = `
+                                    <div id="path">
+                                    <ul id="location"></ul>
+                                    </div>
+                                
+                                
+                                    <main>
+                                        <div id="infoElement">
+                                            <img src="${element.image}" alt="">
+                                            <div id="descri">
+                                                <div id="about">
+                                                    <h1>${element.nom}</h1>
+                                                    <h2>${element.prix}</h2>
+                                                </div>
+                                                <form action="" id="quantity">
+                                                    <div>
+                                                        <p>Taille</p>
+                                                        <select name="" id="">
+                                                            <option value="">S</option>
+                                                            <option value="">L</option>
+                                                            <option value="">M</option>
+                                                        </select>
+                                                    </div>
+                                                    <div>
+                                                        <p>Quantité</p>
+                                                        <input id="inputQuantity" type="number" name="" id="" min=0 value="0">
+                                                    </div>
+                                                    <p id="button">AJOUTER AU PANIER</p>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </main>
+                                `
+                                const button = document.querySelector('#button')
+
+                                button.addEventListener('click', function () {
+
+                                    let articleNbr = parseInt(document.querySelector('#inputQuantity').value)
+
+                                    for (let i = 0; i < articleNbr; i++){
+                                        listAchat.push({"nom":element.nom, "prix":element.prix})
+                                    }
+
+                                    const nbrPanier = document.querySelector('#panierFlottant').querySelector('p')
+                                    const nbrPanier2 = document.querySelector('#nbr')
+
+                                    nbrPanier.innerText = listAchat.length
+                                    nbrPanier2.innerText = listAchat.length
+                                })
+                             }
+                        })
+                    })
+                })
+            })
+        })
+
+        //Page de connection
+
+        const connect = document.querySelectorAll('.connect')
+
+        connect.forEach(element => {
+
+            element.addEventListener('click', function () {
+                variable.innerHTML = pageDeConnection
+            })
+        })
+
+        //Page contenu panier
+        const detailPanier = document.querySelector('#detailPanier')
+
+        detailPanier.addEventListener('click', function () {
+            let list = ""
+            let price = 0
+
+            listAchat.forEach (element => {
+                list += `<li>${element.nom}</li>`
+                price += element.prix
+            })
+            
+            variable.innerHTML = `
+                <main>
+                    <div id="panierContainer">
+                        <div id="listAchat">
+                            <h1>Mon panier</h1>
+                            <ul id="liPanier">
+                                ${list}
+                            </ul>
+                        </div>
+                        <div id="achat">
+                            <div class="prixPanier">
+                                <p>${listAchat.length} articles</p>
+                                <p>${price} €</p>
+                            </div>
+                            <div class="prixPanier">
+                                <p>Total</p>
+                                <p>${price} €</p>
+                            </div>
+                            <p id="button">Check-out</p>
+                        </div>
+                    </div>
+                </main>
+            `
+        })
     })
